7923e3d2b92f10f6b055746a8205c31b849d0de4
Long <longs3515@gmail.com>
2025-12-22 15:24:11 +0700

    Tesst

--- Bai2.java
+++ Bai2.java
@@ -3,6 +3,6 @@ package ss1_java.bai_tap;
 public class Bai2 {
     public static void main(String[] args) {
         System.out.println("C09_Test");
-        System.out.println("Test_2");
+        System.out.println("Test_1");
     }
 }
