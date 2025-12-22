26466f1451505913dc2409e09e406b032f17e888
Long <longs3515@gmail.com>
2025-12-22 11:46:44 +0700

    back up

--- Bai1.java
+++ Bai1.java
@@ -2,6 +2,6 @@ package ss1_java.bai_tap;
 
 public class Bai1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Wold!");
     }
 }
